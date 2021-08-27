803af6f4d1710f1fa52bf763381420023c7a7225
Noah Huetter <noahhuetter@gmail.com>
2021-08-27 09:56:48 +0200

    [CI] Add gitlab CI for ETHZ iis-git

---
+++ .gitlab-ci.d/env-iis.mk
@@ -0,0 +1,7 @@
+
+# Host compilers to use
+CXX=/usr/sepp/bin/g++-9.2.0
+CC=/usr/sepp/bin/gcc-9.2.0
+
+# Utilities
+CMAKE=/home/huettern/.local/bin/cmake
---
+++ .gitlab-ci.d/memora_retry.sh
@@ -0,0 +1,17 @@
+# Copyright 2020 ETH Zurich and University of Bologna.
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#!/usr/bin/env bash
+
+i=1
+max_attempts=10
+while ! memora "$@"; do
+  echo "Attempt $i/$max_attempts of 'memora $@' failed."
+  if test $i -ge $max_attempts; then
+    echo "'memora $@' keeps failing; aborting!"
+    exit 1
+  fi
+  i=$(($i+1))
+done
